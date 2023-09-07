84c039e7e3328fe884aadaf2d157d3846ff03c65
giftty <wwwgift@yahoo.com>
2023-09-07 14:17:40 -0700

    Update changed database dir

--- nazox/settings.py
+++ nazox/settings.py
@@ -118,7 +118,7 @@ WSGI_APPLICATION = 'nazox.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/var/www/html/djangosites/espees-dashboard/db.sqlite3',
     }
 }
 
