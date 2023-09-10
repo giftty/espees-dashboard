291c4267676ef76d8843db287800a7606c8a0e23
giftty <wwwgift@yahoo.com>
2023-09-09 21:44:46 -0700

    database change

--- nazox/settings.py
+++ nazox/settings.py
@@ -116,9 +116,27 @@ WSGI_APPLICATION = 'nazox.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    #  "default": {
+    #     "ENGINE": "django.db.backends.mysql",
+    #      "NAME":"espees_admin_data",
+    #       "USER": "root",
+    #       "PASSWORD": "",
+    #       "HOST": "localhost",
+
+       
+    # }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+         "NAME":"espees_admin_data",
+          "USER": "espees-admin",
+          "PASSWORD": "espees",
+          "HOST": "localhost",
+
+       
     }
 }
 
